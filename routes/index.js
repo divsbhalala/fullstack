1909cd76ff54163ea3cdcbaa41d9c12e7c67a1b6
divs bhalala <bhalaladivs@gmail.com>
2016-08-04 23:11:30 +0530

    Done-Remove /helloworld route and template

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-	res.render('helloworld', { title: 'Hello, World!' })
-});
+
 
 /* GET Userlist page. */
 router.get('/userlist', function(req, res) {
